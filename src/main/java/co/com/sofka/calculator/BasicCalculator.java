0500a051f0b4928e0a6f591110585cf5a6cc8af1
Kitorosano <rosanoesteban2@gmail.com>
2022-03-15 09:59:10 -0300

    Finished sum unit test

---
+++ src/main/java/co/com/sofka/calculator/BasicCalculator.java
@@ -0,0 +1,33 @@
+package co.com.sofka.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+  private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+  public BasicCalculator(){}
+
+  public Long sum(Long number1, Long number2) {
+    logger.info( "Summing {} + {}", number1, number2 );
+    return number1 + number2;
+  }
+
+  public Long sub(Long number1, Long number2) {
+    logger.info( "Subtracting {} - {}", number1, number2 );
+    return number1 - number2;
+  }
+
+  public Long mult(Long number1, Long number2) {
+    logger.info( "Multipliying {} * {}", number1, number2 );
+    return number1 * number2;
+  }
+
+  public Long div(Long number1, Long number2) {
+    logger.info( "Dividing {} / {}", number1, number2 );
+    if(number2 == 0){
+      logger.info( "Cannot divide by zero. {} / {}", number1, number2 );
+    }
+    return number1 / number2;
+  }
+}
